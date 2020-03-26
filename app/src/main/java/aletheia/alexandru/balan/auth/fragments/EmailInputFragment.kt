7de18b6940e500fe842e7ce483e30ec280ec397a
Alexandru Balan <balan.alexandru1997@tutanota.com>
2020-03-26 15:20:17 +0200

    Login and signup inputs now have basic validation

--- app/src/main/java/aletheia/alexandru/balan/auth/fragments/EmailInputFragment.kt
+++ app/src/main/java/aletheia/alexandru/balan/auth/fragments/EmailInputFragment.kt
@@ -2,12 +2,18 @@ package aletheia.alexandru.balan.auth.fragments
 
 import aletheia.alexandru.balan.R
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_email_input.*
 
 class EmailInputFragment : Fragment() {
+
+    private var validEmail : Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +21,32 @@ class EmailInputFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_email_input, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        email_input.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (validEmail) {
+                    email_input.background = activity?.getDrawable(R.drawable.rounded_button_valid)
+                }
+                else {
+                    email_input.background = activity?.getDrawable(R.drawable.rounded_button_invalid)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                email_input.background = activity?.getDrawable(R.drawable.rounded_button_background)
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int){
+
+                validEmail = Regex(Patterns.EMAIL_ADDRESS.toString()) matches s.toString();
+
+            }
+
+        })
     }
 
     companion object {
--- app/src/main/java/aletheia/alexandru/balan/auth/fragments/PasswordInputFragment.kt
+++ app/src/main/java/aletheia/alexandru/balan/auth/fragments/PasswordInputFragment.kt
@@ -1,13 +1,21 @@
 package aletheia.alexandru.balan.auth.fragments
 
 import aletheia.alexandru.balan.R
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_password_input.*
 
 class PasswordInputFragment : Fragment() {
+
+    private val passwordMatcher : Regex = Regex("(?=.*[a-z])(?=.*[A-Z])(?=.*[\\d])(?=.*[~`!@#$%^&*()\\-_+={}\\[\\]|;:\"<>,./?]).{8,}")
+    private var validPassword : Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +23,29 @@ class PasswordInputFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_password_input, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        password_input.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?){
+                if (validPassword) {
+                    password_input.background = activity?.getDrawable(R.drawable.rounded_button_valid)
+                }
+                else {
+                    password_input.background = activity?.getDrawable(R.drawable.rounded_button_invalid)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                password_input.background = activity?.getDrawable(R.drawable.rounded_button_background)
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                validPassword = passwordMatcher matches s.toString()
+            }
+        })
     }
 
     companion object {
