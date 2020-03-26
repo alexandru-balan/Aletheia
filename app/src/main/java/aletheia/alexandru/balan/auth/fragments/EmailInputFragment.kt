2e9165a03cf4667428b3352c7a6a3d6e765521d3
Alexandru Balan <balan.alexandru1997@tutanota.com>
2020-03-26 17:25:45 +0200

    More advanced validation on signup activity

--- app/src/main/java/aletheia/alexandru/balan/auth/fragments/EmailInputFragment.kt
+++ app/src/main/java/aletheia/alexandru/balan/auth/fragments/EmailInputFragment.kt
@@ -13,7 +13,7 @@ import kotlinx.android.synthetic.main.fragment_email_input.*
 
 class EmailInputFragment : Fragment() {
 
-    private var validEmail : Boolean = false
+    private var validEmail: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,27 +26,37 @@ class EmailInputFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        email_input.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                if (validEmail) {
-                    email_input.background = activity?.getDrawable(R.drawable.rounded_button_valid)
+        if (activity?.localClassName == "SignupActivity") {
+
+            email_input.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                    if (validEmail) {
+                        email_input.background =
+                            activity?.getDrawable(R.drawable.rounded_button_valid)
+                    } else {
+                        email_input.background =
+                            activity?.getDrawable(R.drawable.rounded_button_invalid)
+                    }
                 }
-                else {
-                    email_input.background = activity?.getDrawable(R.drawable.rounded_button_invalid)
+
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                    email_input.background =
+                        activity?.getDrawable(R.drawable.rounded_button_background)
                 }
-            }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                email_input.background = activity?.getDrawable(R.drawable.rounded_button_background)
-            }
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int){
+                    validEmail = Regex(Patterns.EMAIL_ADDRESS.toString()) matches s.toString()
 
-                validEmail = Regex(Patterns.EMAIL_ADDRESS.toString()) matches s.toString();
+                }
 
-            }
-
-        })
+            })
+        }
     }
 
     companion object {
--- app/src/main/java/aletheia/alexandru/balan/auth/fragments/PasswordInputFragment.kt
+++ app/src/main/java/aletheia/alexandru/balan/auth/fragments/PasswordInputFragment.kt
@@ -1,14 +1,13 @@
 package aletheia.alexandru.balan.auth.fragments
 
 import aletheia.alexandru.balan.R
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.DrawableRes
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_password_input.*
 
@@ -28,24 +27,56 @@ class PasswordInputFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        password_input.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?){
-                if (validPassword) {
-                    password_input.background = activity?.getDrawable(R.drawable.rounded_button_valid)
-                }
-                else {
-                    password_input.background = activity?.getDrawable(R.drawable.rounded_button_invalid)
-                }
-            }
+        if (activity?.localClassName == "SignupActivity") {
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                password_input.background = activity?.getDrawable(R.drawable.rounded_button_background)
-            }
+            password_input.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                    if (validPassword) {
+                        password_input.background =
+                            activity?.getDrawable(R.drawable.rounded_button_valid)
+                    } else {
+                        password_input.background =
+                            activity?.getDrawable(R.drawable.rounded_button_invalid)
+                    }
+                }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                validPassword = passwordMatcher matches s.toString()
-            }
-        })
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                    password_input.background =
+                        activity?.getDrawable(R.drawable.rounded_button_background)
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    val str = s.toString()
+
+                    val warningText = activity?.findViewById<TextView>(R.id.warning_text)
+
+                    validPassword = passwordMatcher matches str
+                    if (!str.contains(Regex("[\\d]"))) {
+                        warningText?.visibility = View.VISIBLE
+                        warningText?.text = "At least on number is required"
+                    } else if (!str.contains(Regex("[~`!@#$%^&*()\\-_+={}\\[\\]|;:\"<>,./?]"))) {
+                        warningText?.visibility = View.VISIBLE
+                        warningText?.text = "At least one special symbol is required"
+                    } else if (!str.contains(Regex("[A-Z]"))) {
+                        warningText?.visibility = View.VISIBLE
+                        warningText?.text = "At least one upper case character is required"
+                    } else if (!str.contains(Regex("[a-z]"))) {
+                        warningText?.visibility = View.VISIBLE
+                        warningText?.text = "At least one lower case character is required"
+                    } else if (str.length < 8) {
+                        warningText?.visibility = View.VISIBLE
+                        warningText?.text = "The password must have at least 8 characters"
+                    } else {
+                        warningText?.visibility = View.GONE
+                    }
+                }
+            })
+        }
     }
 
     companion object {
