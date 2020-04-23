a47aa74c586085b3d5110b1dee6449a4afad1505
Alexandru Balan <balan.alexandru1997@tutanota.com>
2020-04-23 22:28:13 +0300

    Added main activity
    Signed-off-by: Alexandru Balan <balan.alexandru1997@tutanota.com>

---
+++ app/src/main/java/aletheia/alexandru/balan/MainActivity.kt
@@ -0,0 +1,12 @@
+package aletheia.alexandru.balan
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
